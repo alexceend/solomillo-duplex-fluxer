fb8c44b88ff6af285ade90fa43d0142737e39a5c
alexceend <alexceend@gmail.com>
2026-03-22 02:03:48 +0100

    shigoto

--- commands/work.py
+++ commands/work.py
@@ -7,16 +7,22 @@ from Data_utilities import data_utilities
 
 works = [
     ("Has lamido los zapatos de un empresario, por pena te ha dado ",(50,100)),
-    ("Enhorabuena, has cubrido una baja de 1 mes en el McDonalds, salario: ",(100,200)),
+    ("Enhorabuena, has cubierto una baja de 1 mes en el McDonalds, salario: ",(100,200)),
     ("Has estado todo el día limpiando los parabrisas de los coches en una gasolinera por ",(25,50)),
-    ("Estás de suerte, te has encontrado a Cristiano Ronaldo por la calle y por hacerle una foto te ha dado ",(500,1000))
+    ("Estás de suerte, te has encontrado a Cristiano Ronaldo por la calle y por hacerle una foto te ha dado ",(500,1000)),
+    ("Has estado todo el día aparcando coches (gorrilla) y has ganado ",(10,200))
     ]
 
 @bot.command(name="work")
 async def run(ctx):
-    work, money = get_work()
-    await ctx.send(work + str(money) + f"{currency}")
-    data_utilities.update_balance(str(ctx.author.id), data_utilities.get_balance(ctx.author.id) + money )
+    if can_work():
+        work, money = get_work()
+        await ctx.send(work + str(money) + f"{currency}")
+        data_utilities.update_balance(str(ctx.author.id), data_utilities.get_balance(ctx.author.id) + money )
+
+
+def can_work(last_work):
+    return datetime.now() - 
 
 def get_work():
     #print(datetime.now().total_seconds())
